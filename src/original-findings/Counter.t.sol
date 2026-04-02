644a3b1c43729e4d22330f5680be64c68bf00807
owl <97215481+coolestowl@users.noreply.github.com>
2026-04-02 12:24:20 +0800

    feat: basic

---
+++ src/original-findings/Counter.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+import {console, Test} from "forge-std/Test.sol";
+
+contract CounterTest is Test {
+    uint256 internal constant FORK_BLOCK = 80115970;
+
+    function setUp() public {
+        vm.createSelectFork(vm.envOr("BSC_RPC_URL", string("bsc")), FORK_BLOCK);
+    }
+
+    function testHello() public {
+        console.log(
+            "balance:",
+            address(0x2804ADA1C219E50898e75B2Bd052030580f4fbAC).balance
+        );
+    }
+}
